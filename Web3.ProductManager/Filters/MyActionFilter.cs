b01a0383b56a24db736223cd9efbd47536961a38
greenwormsstudio <greenwormstudio@gmail.com>
2023-09-11 17:45:32 +0700

    Add Project Product

---
+++ Web3.ProductManager/Filters/MyActionFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Web3.ProductManager.Filters
+{
+	public class MyActionFilter: Attribute, IActionFilter
+	{
+		public MyActionFilter()
+		{
+		}
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine(nameof(OnActionExecuted));
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine(nameof(OnActionExecuting));
+        }
+    }
+}
+
---
+++ Web3.ProductManager/Filters/MyResultFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Web3.ProductManager.Filters
+{
+	public class MyResultFilter:Attribute,IResultFilter
+	{
+		public MyResultFilter()
+		{
+		}
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            Console.WriteLine(nameof(OnResultExecuted));
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            Console.WriteLine(nameof(OnResultExecuting));
+        }
+    }
+}
+
