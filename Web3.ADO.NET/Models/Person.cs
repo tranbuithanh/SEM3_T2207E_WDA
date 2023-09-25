6fc0e1b2c7fd354d4cd1c43335ff8971da83e8a0
nghia.ht <nghia.ht@evotek.vn>
2023-09-25 14:51:31 +0700

    first commit

--- Web3.ADO.NET/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web3.ADO.NET.Models
-{
-    public class Person
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Title { get; set; }
-    }
-}
