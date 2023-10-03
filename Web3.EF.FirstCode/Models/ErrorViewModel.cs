7a03e9d5b0af7f702cbc6829a5fb207fcd26433a
tranbuithanh <109666928+tranbuithanh@users.noreply.github.com>
2023-10-03 23:11:43 +0700

    Tran Bui Thanh Nop ASM

--- Web3.EF.FirstCode/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web3.EF.FirstCode.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
